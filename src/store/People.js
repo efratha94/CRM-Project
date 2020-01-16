469ea947ed1a931e667379c0f912a14b96919dc7
Efrat Harel <efratha94@gmail.com>
2020-01-16 11:27:30 +0200

    crm project setup

--- src/store/People.js
+++ src/store/People.js
@@ -2,4 +2,4 @@ const Sequelize = require("sequelize")
 const sequelize = new Sequelize('mysql://root:tedds19942@localhost/crm_project')
 const clientData = require("../../react-crm-ex-efratha94/data")
 
-
+// console.log(clientData)
